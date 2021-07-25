78beb01a1701f28491e27a4b37c70c7cccc5d2ff
mark <markel20002000@gmail.com>
2021-07-25 13:27:12 +0600

    добавил скрипты для добавления преподов в БД

--- src/univer_api/scripts.py
+++ src/univer_api/scripts.py
@@ -2,6 +2,7 @@ import csv
 from os import PathLike
 from typing import Union
 
+import sqlalchemy.exc
 from sqlalchemy.orm import Session
 
 from univer_api import tables
@@ -13,25 +14,51 @@ def insert_subject_names(
 ):
     with open(csv_file, "r", encoding="utf-8") as f:
         reader = csv.DictReader(f)
-        subject_names = [row["name"] for row in reader]
-        print(f"Список предметов: {subject_names}")
-
-    for name in subject_names:
-        new_name = tables.SubjectName(name=name)
-        session.add(new_name)
-    session.commit()
+        for row in reader:
+            name = row["name"]
+            print(name)
+            subject_name = tables.SubjectName(name=name)
+            session.add(subject_name)
+            try:
+                session.commit()
+            except sqlalchemy.exc.IntegrityError:
+                print(f"{subject_name.name} уже в БД")
+                session.rollback()
 
 
 def insert_groups(
         csv_file: Union[PathLike, str],
         session: Session,
-
 ):
     with open(csv_file, "r", encoding="utf-8") as f:
         reader = csv.DictReader(f)
-        group_names = [row["name"] for row in reader]
-        print(f"Список групп: {group_names}")
-    for group in group_names:
-        new_group = tables.Group(name=group)
-        session.add(new_group)
-    session.commit()
+        for row in reader:
+            name = row["name"]
+            print(name)
+            group = tables.Group(name=name)
+            session.add(group)
+            try:
+                session.commit()
+            except sqlalchemy.exc.IntegrityError:
+                print(f"{group.name} уже в БД")
+                session.rollback()
+
+
+def insert_teachers(
+        csv_file: Union[PathLike, str],
+        session: Session,
+):
+    with open(csv_file, "r", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            first_name = row["first_name"].strip()
+            second_name = row["second_name"].strip()
+            middle_name = row["middle_name"].strip()
+            print(second_name, first_name, middle_name)
+            teacher = tables.Teacher(first_name=first_name, second_name=second_name, middle_name=middle_name)
+            session.add(teacher)
+            try:
+                session.commit()
+            except sqlalchemy.exc.IntegrityError:
+                print(f"{teacher.second_name} уже в БД")
+                session.rollback()
