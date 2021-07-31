50611954b85beb15694716579f7fd2ed15134e7b
mark <markel20002000@gmail.com>
2021-07-31 14:01:30 +0600

    Изменил расположения файла с БД

--- src/univer_api/settings.py
+++ src/univer_api/settings.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     server_host: str = 'localhost'
     server_port: int = 5050
 
-    database_url: str = "sqlite:///./db.sqlite3"
+    database_url: str = "sqlite:///../db.sqlite3"
 
 
 settings = Settings()
