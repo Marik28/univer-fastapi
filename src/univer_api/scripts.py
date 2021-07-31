cc30b8528c118cc5858e5abce8dad21605dc65d2
mark <markel20002000@gmail.com>
2021-07-31 14:01:12 +0600

    Добавил флаги для запуска scripts.py, а также немного изменил импорты. Теперь scripts.py можно пользоваться как консольной утилитой

--- src/univer_api/scripts.py
+++ src/univer_api/scripts.py
@@ -1,101 +1,122 @@
+import argparse
 import csv
 import datetime as dt
 from os import PathLike
 from typing import Union
 
-import sqlalchemy.exc
-from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
 
-from univer_api import tables
+from . import tables
+from .database import Session
+
+db_tables = ["lessons", "subjects", "teachers", "groups"]
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", help="Функция, которую необходимо вызвать", choices=db_tables)
+parser.add_argument("-f", help="Путь до csv файла, из которого будут браться данные")
 
 
 def insert_subjects(
         csv_file: Union[PathLike, str],
-        session: Session,
 ):
-    with open(csv_file, "r", encoding="utf-8") as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            name = row["name"]
-            print(name)
-            subject = tables.Subject(name=name)
-            session.add(subject)
-            try:
-                session.commit()
-            except sqlalchemy.exc.IntegrityError:
-                print(f"{subject.name} уже в БД")
-                session.rollback()
+    with Session() as session:
+        with open(csv_file, "r", encoding="utf-8") as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                name = row["name"]
+                print(name)
+                subject = tables.Subject(name=name)
+                session.add(subject)
+                try:
+                    session.commit()
+                except IntegrityError:
+                    print(f"{subject.name} уже в БД")
+                    session.rollback()
 
 
 def insert_groups(
         csv_file: Union[PathLike, str],
-        session: Session,
 ):
-    with open(csv_file, "r", encoding="utf-8") as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            name = row["name"]
-            print(name)
-            group = tables.Group(name=name)
-            session.add(group)
-            try:
-                session.commit()
-            except sqlalchemy.exc.IntegrityError:
-                print(f"{group.name} уже в БД")
-                session.rollback()
+    with Session() as session:
+        with open(csv_file, "r", encoding="utf-8") as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                name = row["name"]
+                print(name)
+                group = tables.Group(name=name)
+                session.add(group)
+                try:
+                    session.commit()
+                except IntegrityError:
+                    print(f"{group.name} уже в БД")
+                    session.rollback()
 
 
 def insert_teachers(
         csv_file: Union[PathLike, str],
-        session: Session,
 ):
-    with open(csv_file, "r", encoding="utf-8") as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            first_name = row["first_name"].strip()
-            second_name = row["second_name"].strip()
-            middle_name = row["middle_name"].strip()
-            print(second_name, first_name, middle_name)
-            teacher = tables.Teacher(first_name=first_name, second_name=second_name, middle_name=middle_name)
-            session.add(teacher)
-            try:
-                session.commit()
-            except sqlalchemy.exc.IntegrityError:
-                print(f"{teacher.second_name} уже в БД")
-                session.rollback()
+    with Session() as session:
+        with open(csv_file, "r", encoding="utf-8") as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                first_name = row["first_name"].strip()
+                second_name = row["second_name"].strip()
+                middle_name = row["middle_name"].strip()
+                print(second_name, first_name, middle_name)
+                teacher = tables.Teacher(first_name=first_name, second_name=second_name, middle_name=middle_name)
+                session.add(teacher)
+                try:
+                    session.commit()
+                except IntegrityError:
+                    print(f"{teacher.second_name} уже в БД")
+                    session.rollback()
 
 
 def insert_lessons(
         csv_file: Union[PathLike, str],
-        session: Session,
 ):
-    with open(csv_file, "r", encoding="utf-8") as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            teacher_second_name = row["teacher"].split()[0].strip()
-            name = row["name"].strip()
-            parity = int(row["parity"])
-            group_name = row["group"].strip()
-            subgroup = int(row["subgroup"])
-            time = dt.time.fromisoformat(row["time"].strip())
-            kind = row["kind"].strip()
-            day = int(row["day"])
+    with Session() as session:
+        with open(csv_file, "r", encoding="utf-8") as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                teacher_second_name = row["teacher"].split()[0].strip()
+                name = row["name"].strip()
+                parity = int(row["parity"])
+                group_name = row["group"].strip()
+                subgroup = int(row["subgroup"])
+                time = dt.time.fromisoformat(row["time"].strip())
+                kind = row["kind"].strip()
+                day = int(row["day"])
 
-            teacher = (session.query(tables.Teacher)
-                       .filter(tables.Teacher.second_name == teacher_second_name)
-                       .first())
-            subject = (session.query(tables.Subject)
-                       .filter(tables.Subject.name == name)
-                       .first())
-            group = (session.query(tables.Group)
-                     .filter(tables.Group.name == group_name)
-                     .first())
-            lesson = tables.Lesson(subject=subject, teacher=teacher, kind=kind, time=time, parity=parity, group=group,
-                                   subgroup=subgroup, day=day)
-            session.add(lesson)
-            print(f"{subject.name} ({group.name})")
-            try:
-                session.commit()
-            except sqlalchemy.exc.IntegrityError:
-                print(f"{lesson.subject.name} ({lesson.day} {lesson.time}) уже в БД")
-                session.rollback()
+                teacher = (session.query(tables.Teacher)
+                           .filter(tables.Teacher.second_name == teacher_second_name)
+                           .first())
+                subject = (session.query(tables.Subject)
+                           .filter(tables.Subject.name == name)
+                           .first())
+                group = (session.query(tables.Group)
+                         .filter(tables.Group.name == group_name)
+                         .first())
+                lesson = tables.Lesson(subject=subject, teacher=teacher, kind=kind, time=time, parity=parity,
+                                          group=group,
+                                          subgroup=subgroup, day=day)
+                session.add(lesson)
+                print(f"{subject.name} ({group.name})")
+                try:
+                    session.commit()
+                except IntegrityError:
+                    print(f"{lesson.subject.name} ({lesson.day} {lesson.time}) уже в БД")
+                    session.rollback()
+
+
+if __name__ == '__main__':
+    flag_to_function = {
+        db_tables[0]: insert_lessons,
+        db_tables[1]: insert_subjects,
+        db_tables[2]: insert_teachers,
+        db_tables[3]: insert_groups,
+    }
+    args = parser.parse_args()
+    table = args.t
+    file = args.f
+    print('-------', table, file)
+    flag_to_function[table](file)
