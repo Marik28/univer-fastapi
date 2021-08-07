fa2281a3ee4bc6839940993e5f1d10230fd944ea
mark <markel20002000@gmail.com>
2021-08-07 21:49:41 +0600

    добавил скрипт для добавления данных для примера в бд

---
+++ src/univer_api/scripts/create_example.py
@@ -0,0 +1,11 @@
+import os
+
+from ..settings import settings
+
+if __name__ == '__main__':
+    from .add_table import flag_to_function
+
+    for dirpath, _, filenames in os.walk(settings.base_dir / "data" / "example"):
+        for filename in filenames:
+            name = filename.split(".")[0]
+            flag_to_function[name](f"{dirpath}/{filename}")
