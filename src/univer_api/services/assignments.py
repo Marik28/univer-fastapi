b0d892fb84b79cdcb878b08458aa693b4b0b9717
Marik28 <markel20002000@gmail.com>
2021-09-12 19:19:48 +0600

    добавил сервис для заданий

---
+++ src/univer_api/services/assignments.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+from fastapi import Depends
+from sqlalchemy.orm import Session
+
+from .. import tables
+from ..database import get_session
+
+
+class AssignmentsService:
+    def __init__(self, session: Session = Depends(get_session)):
+        self.session = session
+
+    def get_list(
+            self,
+            group: Optional[str] = None,
+            subgroup: Optional[str] = None,
+            subject: Optional[str] = None,
+    ) -> list[tables.Assignment]:
+        query = self.session.query(tables.Assignment).join(tables.Assignment.group).join(tables.Assignment.subject)
+
+        if group is not None:
+            query = query.filter(tables.Group.name == group)
+
+        if subject is not None:
+            query = query.filter(tables.Subject.name == subject)
+
+        return query.order_by(tables.Group.name.asc(), tables.Assignment.complete_before.asc()).all()
