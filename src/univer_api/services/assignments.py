612b9ff895cd402dbcf84cefa771ecfb195e6663
Marik28 <markel20002000@gmail.com>
2021-09-13 00:25:38 +0600

    добавил наследование от класса GroupFilterHelper

--- src/univer_api/services/assignments.py
+++ src/univer_api/services/assignments.py
@@ -4,10 +4,11 @@ from fastapi import Depends
 from sqlalchemy.orm import Session
 
 from .. import tables
+from ..api.base import GroupFilterHelper
 from ..database import get_session
 
 
-class AssignmentsService:
+class AssignmentsService(GroupFilterHelper):
     def __init__(self, session: Session = Depends(get_session)):
         self.session = session
 
@@ -19,10 +20,9 @@ class AssignmentsService:
     ) -> list[tables.Assignment]:
         query = self.session.query(tables.Assignment).join(tables.Assignment.group).join(tables.Assignment.subject)
 
-        if group is not None:
-            query = query.filter(tables.Group.name == group)
-
         if subject is not None:
             query = query.filter(tables.Subject.name == subject)
+
+        query = self.filter_group_and_subgroup(query, group, subgroup)
 
         return query.order_by(tables.Group.name.asc(), tables.Assignment.complete_before.asc()).all()
