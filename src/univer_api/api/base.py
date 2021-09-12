41e33e8b0cd9226b2fba8129772a27b3486f3413
Marik28 <markel20002000@gmail.com>
2021-09-12 20:13:19 +0600

    вынес логику фильтрации группы и подгруппы в отдельный класс для переиспользования

---
+++ src/univer_api/api/base.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+import sqlalchemy.orm
+
+from univer_api import tables
+from univer_api.models.groups import Subgroup
+
+
+class GroupFilterHelper:
+    subgroup_exclusion_dict = {
+        Subgroup.FIRST_GROUP.value: Subgroup.SECOND_GROUP.value,
+        Subgroup.SECOND_GROUP.value: Subgroup.FIRST_GROUP.value,
+    }
+
+    def exclude_subgroup(self, subgroup: int) -> int:
+        return self.subgroup_exclusion_dict[subgroup]
+
+    def filter_group_and_subgroup(self, query: sqlalchemy.orm.Query, group: Optional[str], subgroup: Optional[int]):
+        if group is not None:
+            query = query.filter(tables.Group.name == group)
+            if subgroup is not None and subgroup != Subgroup.BOTH:
+                subgroup_to_exclude = self.exclude_subgroup(subgroup)
+                query = query.filter(tables.Lesson.subgroup != subgroup_to_exclude)
+        return query
