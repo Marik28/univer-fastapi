4f313499a954eec025d58bb3a79e610c35d2e6c7
Marik28 <markel20002000@gmail.com>
2021-09-12 18:53:09 +0600

    перенес скрипты, в src, т.к. они не относятся к коду напрямую

--- src/scripts/create_db.py
+++ src/scripts/create_db.py
@@ -1,5 +1,5 @@
-from ..tables import Base
-from ..database import engine
+from univer_api.tables import Base
+from univer_api.database import engine
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
