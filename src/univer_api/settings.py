543f17dfdc59687fab6d4e4752a882bbf40cb651
Marik28 <markel20002000@gmail.com>
2021-09-12 23:57:10 +0600

    изменил стандартное значение database_url

--- src/univer_api/settings.py
+++ src/univer_api/settings.py
@@ -7,9 +7,9 @@ class Settings(BaseSettings):
     server_host: str = 'localhost'
     server_port: int = 5050
 
-    database_url: str = "sqlite:///../db.sqlite3"
-
     base_dir: Path = Path(__file__).resolve().parent.parent.parent
+
+    database_url: str = f"sqlite:///{base_dir / 'db.sqlite3'}"
 
 
 settings = Settings()
