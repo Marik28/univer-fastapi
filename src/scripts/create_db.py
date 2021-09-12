a4704d3be47cb7a20b610e8e265722e79021b95c
Marik28 <markel20002000@gmail.com>
2021-09-12 18:56:39 +0600

    переход на использование sqlalchemy_utils, чтобы не зависеть то конкретной бд

--- src/scripts/create_db.py
+++ src/scripts/create_db.py
@@ -1,6 +1,11 @@
-from univer_api.tables import Base
+from sqlalchemy_utils.functions import database_exists, create_database
+
 from univer_api.database import engine
+from univer_api.tables import Base
 
 if __name__ == '__main__':
-    Base.metadata.create_all(engine)
-    print("БД успешно создана")
+    db_url = engine.url
+    if not database_exists(db_url):
+        create_database(db_url)
+        Base.metadata.create_all(engine)
+        print("БД успешно создана")
