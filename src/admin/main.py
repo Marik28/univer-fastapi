b148edf27237f83707f8dd389d233fa8ceaea530
mark <markel20002000@gmail.com>
2021-09-16 20:47:02 +0600

    добавил базовый url для админки

--- src/admin/main.py
+++ src/admin/main.py
@@ -1,12 +1,13 @@
 from flask_admin import Admin
 
 from univer_api.database import Session
+from univer_api.settings import settings
 from univer_api.tables import Assignment, UsefulLink
 from .app import app
 from .views.assignments import AssignmentsView
 from .views.subjects import UsefulLinksView
 
-admin = Admin(app, name="univer_api", template_mode="bootstrap4")
+admin = Admin(app, name="univer_api", template_mode="bootstrap4", url=settings.admin_url)
 
 with Session() as session:
     admin.add_view(AssignmentsView(Assignment, session))
