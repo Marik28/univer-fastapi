d8d07274cc4128afd19a00e371e8131cbe95a57c
mark <markel20002000@gmail.com>
2021-09-16 20:10:34 +0600

    уже поплава, не знаю, как правильно раскидать по файлам

--- src/admin/main.py
+++ src/admin/main.py
@@ -11,4 +11,5 @@ admin = Admin(app, name="univer_api", template_mode="bootstrap4")
 with Session() as session:
     admin.add_view(AssignmentsView(Assignment, session))
     admin.add_view(UsefulLinksView(UsefulLink, session))
-    app.run()
+    if __name__ == '__main__':
+        app.run()
