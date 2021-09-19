133a81307c809a9620ac038443aa99a1bfb15a0b
mark <markel20002000@gmail.com>
2021-09-19 12:35:39 +0600

    добавил фильтрацию предметов по группам

--- src/univer_api/api/subjects.py
+++ src/univer_api/api/subjects.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 
 from ..models.subjects import Subject
 from ..services.subjects import SubjectsService
@@ -10,5 +10,6 @@ router = APIRouter(prefix="/subjects")
 async def get_subjects_list(
         service: SubjectsService = Depends(SubjectsService),
         with_links_only: bool = False,
+        group: str = Query(None, example="ЭЭ-18-4"),
 ):
-    return service.get_list(with_links_only)
+    return service.get_list(with_links_only, group)
--- src/univer_api/services/subjects.py
+++ src/univer_api/services/subjects.py
@@ -11,8 +11,13 @@ class SubjectsService:
     def __init__(self, session: sqlalchemy.orm.Session = Depends(get_session)):
         self.session = session
 
-    def get_list(self, with_links_only: Optional[bool]) -> list[tables.Subject]:
-        query = self.session.query(tables.Subject)
+    def get_list(self, with_links_only: Optional[bool], group: Optional[str]) -> list[tables.Subject]:
+        query = (
+            self.session.query(tables.Subject)
+                .join(tables.Lesson)
+                .join(tables.Lesson.group)
+                .filter(tables.Group.name == group)
+        )
         if with_links_only:
             query = query.join(tables.Subject.useful_links)
         return query.all()
