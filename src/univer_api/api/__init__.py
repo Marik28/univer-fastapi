16f4ca620240518db4bf16b38d0bda2214297143
mark <markel20002000@gmail.com>
2021-10-03 19:02:25 +0600

    добавил эндпоинты для работы со студентами

--- src/univer_api/api/__init__.py
+++ src/univer_api/api/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from . import groups, lessons, teachers, assignments, subjects
+from . import groups, lessons, teachers, assignments, subjects, students
 
 router = APIRouter()
 router.include_router(lessons.router)
@@ -8,3 +8,4 @@ router.include_router(groups.router)
 router.include_router(teachers.router)
 router.include_router(assignments.router)
 router.include_router(subjects.router)
+router.include_router(students.router)
