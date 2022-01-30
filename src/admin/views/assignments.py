5705ceda7cf0cb33d369934b3b0e16e90b16d004
mark <markel20002000@gmail.com>
2022-01-30 14:04:24 +0600

    add customization

--- src/admin/views/assignments.py
+++ src/admin/views/assignments.py
@@ -8,14 +8,16 @@ from ..services import make_choices
 class AssignmentsView(BaseModelView):
     # templates
     list_template = "assignments/list.html"
+    details_template = "assignments/details.html"
 
     # columns
-    column_list = ["title", "subject", "complete_before", "is_important", "archived"]
+    column_list = ["title", "subject", "group", "complete_before", "archived"]
     column_editable_list = ["archived"]
 
     # formatters
     column_formatters = {
         "subject": macro("render_subject"),
+        "group": macro("render_group"),
     }
 
     # customization
