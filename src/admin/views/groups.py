3bed7167f078b6a6bf69dd5bc716ca891f13bf63
mark <markel20002000@gmail.com>
2022-01-30 13:32:06 +0600

    add details.html

--- src/admin/views/groups.py
+++ src/admin/views/groups.py
@@ -1,5 +1,16 @@
+from flask_admin.model.template import macro
+
 from .base import BaseModelView
 
 
 class GroupsBaseView(BaseModelView):
-    form_excluded_columns = ["lessons", "assignments"]
+    # templates
+    details_template = "groups/details.html"
+
+    # columns
+    form_excluded_columns = ["lessons", "assignments", "students"]
+    column_details_list = ["name", "assignments"]
+    # formatters
+    column_formatters_detail = {
+        "assignments": macro("render_assignments"),
+    }
