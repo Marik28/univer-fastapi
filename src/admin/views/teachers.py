0f53149053b2193306b8194981a94bb9df407c92
mark <markel20002000@gmail.com>
2022-01-30 13:41:48 +0600

    add customization

--- src/admin/views/teachers.py
+++ src/admin/views/teachers.py
@@ -2,4 +2,11 @@ from .base import BaseModelView
 
 
 class TeachersView(BaseModelView):
+    # columns
     form_excluded_columns = ["lessons"]
+    column_list = ["second_name", "first_name", "middle_name"]
+    column_details_list = ["second_name", "first_name", "middle_name"]
+
+    # customization
+    column_default_sort = "second_name"
+    column_searchable_list = ["second_name", "first_name", "middle_name"]
